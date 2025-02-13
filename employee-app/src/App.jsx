8a0850b83be253b952cd0ea6aa5e8c5f74547a08
DanKazuky <daguazacot@ucentral.edu.co>
2025-02-13 17:44:30 -0500

    feat: Creacion y cambio de tabla consulta empleado

--- employee-app/src/App.jsx
+++ employee-app/src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import EmployeeLookup from "./components/EmployeeLookup";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div className="p-10">
-        <h1 className="text-2xl font-bold mb-4">Consulta de Empleados</h1>
+        <h1 className="text-2xl font-bold mb-4">Employee Manager</h1>
         <EmployeeLookup />
       </div>
 
